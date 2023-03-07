e759552feca3e9f893f55aa260e720854477f0f2
Aofject <aof@kos.co.th>
2023-03-07 12:17:42 +0700

    feat: add type card.mock

--- src/mockups/card.mock.ts
+++ src/mockups/card.mock.ts
@@ -5,27 +5,32 @@
     ? arrow function generic if you want to use -> https://timmousk.com/blog/typescript-arrow-function-generic
 */
 
-const isActive = true
-const isUnActive = "false"
-const price = 1.5
-const thb = 30.5
-const cardName = "Son Heung-Min"
-const cardNameList = ["Kevin", "Ronaldo"]
-const randomValue = ["Van", 109, "Milner"]
-const date1 = "2022-11-29T09:21:02.967Z"
-const date2 = "Tue Nov 29 2022 16:21:02 GMT+0700"
-const date3 = new Date()
-const date4 = new Date(date1)
-const date5 = new Date(date2)
-const cardType = null
-const cardTier = undefined
+// any, unknow, never -> ไม่ดี
+const isActive: boolean = false // boolean -> true / false
+const isUnActive: string = "false"
+const price: number = 1.5 // float -> 0.0 , int -> 0
+const thb: number = 30.5
+const cardName: string = "Son Heung-Min"
+// const cardNameList: string[] = ["Kevin", "Ronaldo"]
+const cardNameList: Array<string> = ["Kevin", "Ronaldo"]
+// const randomValue: (string | number)[] = ["Van", 109, "Milner", "", 1234]
+const randomValue: Array<string | number> = ["Van", 109, "Milner", "", 1234]
+const date1: string = "2022-11-29T09:21:02.967Z"
+const date2: string = "Tue Nov 29 2022 16:21:02 GMT+0700"
+const date3: Date = new Date()
+const date4: Date = new Date(date1)
+const date5: Date = new Date(date2)
+const cardType: null = null
+const cardTier: undefined = undefined
 const _unk: unknown = "Kaoru"
 
-type TNum = 10 | 20
+type Role = "user" | "admin"
+
+const x: Role = "admin"
 
 const setFakeMock = () => {
   let fake: number = 10
-  if (price > 0) {
+  if (price > thb) {
     fake = 20
   }
 }
